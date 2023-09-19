1b725b30150f858928cc54bdf652fdbcb6c22f60
Patric Keisala <finne_no1@hotmail.com>
2023-09-19 11:23:34 +0200

    Renamed folder.

---
+++ docs/_app/immutable/nodes/1.6b2e261a.js
@@ -0,0 +1,1 @@
+import{s as t}from"../chunks/scheduler.66141ec3.js";import{S as r,i as s}from"../chunks/index.b5e81050.js";import"../chunks/paths.2fac5576.js";class l extends r{constructor(o){super(),s(this,o,null,null,t,{})}}export{l as component};
