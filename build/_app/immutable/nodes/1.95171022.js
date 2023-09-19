70e7e9bebb19338b8a49b844c74b6dcfab99916d
Patric Keisala <finne_no1@hotmail.com>
2023-09-19 11:45:37 +0200

    Removing build from github

--- build/_app/immutable/nodes/1.95171022.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"../chunks/scheduler.66141ec3.js";import{S as r,i as s}from"../chunks/index.b5e81050.js";import"../chunks/paths.938f4750.js";class l extends r{constructor(o){super(),s(this,o,null,null,t,{})}}export{l as component};
