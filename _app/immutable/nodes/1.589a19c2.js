9c52e829fe0b41c402af950f86756e55f7ed2a40
Patric Keisala <finne_no1@hotmail.com>
2025-01-07 21:08:12 +0100

    Updates

--- _app/immutable/nodes/1.589a19c2.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"../chunks/scheduler.8fa37b9d.js";import{S as r,i as s}from"../chunks/index.fbb25e7c.js";import"../chunks/paths.8e8cbccb.js";class l extends r{constructor(o){super(),s(this,o,null,null,t,{})}}export{l as component};
